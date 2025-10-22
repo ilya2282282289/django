040e68bf69412a2145844f220f258aef3d361581
ilya2282282289 <125489074+ilya2282282289@users.noreply.github.com>
2025-10-22 21:39:55 +0300

    Add files via upload

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import index, dungion, minecraft
+from .views import index, dungion, minecraft, legends, earth, story_mode, story_mode_s2
 
 urlpatterns = [
+    path('', index, name='home'),
     path('home', index, name='index'),
     path('minecraft_dungion', dungion, name='dungion'),
     path('minecraft', minecraft, name='minecraft'),
+    path('legends', legends, name='legends'),
+    path('earth', earth, name='earth'),
+    path('story', story_mode, name='story'),
+    path('storys2', story_mode_s2, name='storys2'),
 ]
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -9,3 +9,15 @@ def dungion(request):
 
 def minecraft(request):
     return render(request, 'minecraft.html')
+
+def legends(request):
+    return render(request, 'legends.html')
+
+def earth(request):
+    return render(request, 'minecraft_earth.html')
+
+def story_mode(request):
+    return render(request, 'minecraft_story_mode.html')
+
+def story_mode_s2(request):
+    return render(request, 'minecraft_story_mode_s2.html')
